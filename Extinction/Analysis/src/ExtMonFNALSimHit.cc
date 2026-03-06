297b3d9e9ffeb20fb4e86e6316c7e53e1bb26d0a
caohc <caohc0311@gmail.com>
2026-03-05 20:49:14 -0600

    Adding process code for Extinction Monitor Analysis

---
+++ Extinction/Analysis/src/ExtMonFNALSimHit.cc
@@ -0,0 +1,21 @@
+#include "Offline/MCDataProducts/inc/ExtMonFNALSimHit.hh"
+#include <ostream>
+
+namespace mu2e {
+  void  ExtMonFNALSimHit::setSimParticle(const art::Ptr<SimParticle>& p) {
+    particle_ = p;
+  }
+
+  std::ostream& operator<<(std::ostream& os, const ExtMonFNALSimHit& hit) {
+    return os<<"ExtMonFNALSimHit(sid="<<hit.moduleId()
+             <<", PID="<<hit.simParticle()->pdgId()
+	     <<", particle="<<hit.simParticle()->id()
+             <<", eTot="<<hit.totalEnergyDeposit()
+             <<", eIon="<<hit.ionizingEnergyDeposit()
+             <<", startPos="<<hit.localStartPosition()
+             <<", startTime="<<hit.startTime()
+             <<", endPos="<<hit.localEndPosition()
+             <<", endTime="<<hit.endTime()
+             <<")";
+  }
+}
